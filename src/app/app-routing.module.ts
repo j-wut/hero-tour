0b187d38cd7c9eba742fd57432401920ac9f4fda
James Wu <james.wut@gmail.com>
2019-08-27 11:23:15 -0400

    adding dashboard component, adding css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { HeroDetailsComponent } from "./hero-details/hero-details.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 const routes: Routes = [
+  { path: "", component: DashboardComponent },
   { path: "list", component: HeroesComponent },
   { path: "details/:id", component: HeroDetailsComponent }
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HeroService } from "../hero.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  heroes;
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
+    this.heroes = this.heroService
+      .getHeroes()
+      .slice()
+      .sort((h1, h2) => {
+        return h2.id - h1.id;
+      })
+      .slice(0, 4);
   }
-
 }
